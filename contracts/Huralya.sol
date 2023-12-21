087f427f279f14fd402d59b6420cf0f70aabaf6a
Kasra <zoshitam@gmail.com>
2023-12-21 19:03:20 +0330

    update huralya contract logic

--- contracts/Huralya.sol
+++ contracts/Huralya.sol
@@ -5,48 +5,31 @@ import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import '@openzeppelin/contracts/access/Ownable2Step.sol';
 
 contract Huralya is ERC20, Ownable2Step {
-  uint256 public constant MINTING_INTERVAL = 365 * 4 days;
+  uint256 public constant MINT_INTERVAL = 365 days * 4;
 
-  uint256 public constant TOTAL_SUPPLY_LIMIT_PER_4_YEARS =
-    100_000_000 * 10 ** 6;
-  uint256 public MAX_MINTED_AMOUNT_AFTER_DEPLOY;
+  uint256 public constant MAX_SUPPLY = 200_000_000 * 10 ** 6;
 
-  uint256 private deployTimestamp;
+  uint256 public MINT_TIMESTAP;
 
-  constructor() ERC20('Huralya', 'LYA') Ownable(msg.sender) {
-    deployTimestamp = block.timestamp;
-    _mint(owner(), TOTAL_SUPPLY_LIMIT_PER_4_YEARS);
+  constructor() ERC20('Huralya', 'LYA') {
+    MINT_TIMESTAP = block.timestamp + MINT_INTERVAL;
+    _mint(owner(), 100_000_000 * 10 ** 6);
   }
 
   function burn(uint256 _amount) external {
     super._burn(msg.sender, _amount);
   }
 
-  function mint(uint256 _amount) external onlyOwner {
-    uint256 currentMintableAmount = getMintableAmount();
+  function mint() external onlyOwner {
     require(
-      _amount <= currentMintableAmount,
-      'LYA: your entered amount is greater than the mintable limit'
+      block.timestamp >= MINT_TIMESTAP,
+      'LYA: Minting is not allowed yet, please wait until the next minting interval'
     );
-
-    super._mint(owner(), _amount);
-    MAX_MINTED_AMOUNT_AFTER_DEPLOY = MAX_MINTED_AMOUNT_AFTER_DEPLOY + _amount;
-  }
-
-  function getMintableAmount() public view returns (uint256) {
-    uint256 elapsedMintingTime = block.timestamp - deployTimestamp;
-    uint256 mintingPeriods = elapsedMintingTime / MINTING_INTERVAL;
-
-    uint256 remainingTimeInInterval = elapsedMintingTime % MINTING_INTERVAL;
-    uint256 maxMintableAmount = (mintingPeriods *
-      MINTING_INTERVAL *
-      TOTAL_SUPPLY_LIMIT_PER_4_YEARS) /
-      (MINTING_INTERVAL + remainingTimeInInterval);
-
-    if (maxMintableAmount > MAX_MINTED_AMOUNT_AFTER_DEPLOY) {
-      return maxMintableAmount - MAX_MINTED_AMOUNT_AFTER_DEPLOY;
-    }
-    return MAX_MINTED_AMOUNT_AFTER_DEPLOY - maxMintableAmount;
+    require(
+      totalSupply() < MAX_SUPPLY,
+      'LYA: Maximum supply reached, no more tokens can be minted'
+    );
+    super._mint(owner(), 100_000_000 * 10 ** 6);
   }
 
   function decimals() public view virtual override returns (uint8) {
