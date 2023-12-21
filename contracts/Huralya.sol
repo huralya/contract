adba24913e7bbec1dc31fd411ac5787770e3d52d
huralya <88592854+huralya@users.noreply.github.com>
2023-12-22 00:09:24 +0330

    Update Huralya.sol

--- contracts/Huralya.sol
+++ contracts/Huralya.sol
@@ -1,38 +1,39 @@
-//SPDX-License-Identifier: Huralya
+// SPDX-License-Identifier: Huralya
 pragma solidity 0.8.23;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import '@openzeppelin/contracts/access/Ownable2Step.sol';
 
 contract Huralya is ERC20, Ownable2Step {
-  uint256 public constant MINT_INTERVAL = 365 days * 4;
+    uint256 public constant MINT_INTERVAL = 365 days * 4;
+    uint256 public constant MAX_SUPPLY = 200_000_000 * 10 ** 6;
 
-  uint256 public constant MAX_SUPPLY = 200_000_000 * 10 ** 6;
+    uint256 public lastMintTimestamp;
 
-  uint256 public MINT_TIMESTAP;
+    constructor() ERC20('Huralya', 'LYA') {
+        
+        _mint(owner(), 100_000_000 * 10 ** 6);
+        lastMintTimestamp = block.timestamp;
+    }
 
-  constructor() ERC20('Huralya', 'LYA') {
-    MINT_TIMESTAP = block.timestamp + MINT_INTERVAL;
-    _mint(owner(), 100_000_000 * 10 ** 6);
-  }
+    function mint(uint256 _amount) external onlyOwner {
+        require(
+            block.timestamp >= lastMintTimestamp + MINT_INTERVAL,
+            'LYA: Minting is not allowed yet, please wait until the next minting interval'
+        );
+        require(
+            totalSupply() + _amount <= MAX_SUPPLY,
+            'LYA: Cannot mint beyond maximum supply'
+        );
 
-  function burn(uint256 _amount) external {
-    super._burn(msg.sender, _amount);
-  }
+        _mint(owner(), _amount);
+    }
 
-  function mint() external onlyOwner {
-    require(
-      block.timestamp >= MINT_TIMESTAP,
-      'LYA: Minting is not allowed yet, please wait until the next minting interval'
-    );
-    require(
-      totalSupply() < MAX_SUPPLY,
-      'LYA: Maximum supply reached, no more tokens can be minted'
-    );
-    super._mint(owner(), 100_000_000 * 10 ** 6);
-  }
+    function decimals() public view virtual override returns (uint8) {
+        return 6;
+    }
 
-  function decimals() public view virtual override returns (uint8) {
-    return 6;
-  }
+    function burn(uint256 _amount) external {
+        super._burn(msg.sender, _amount);
+    }
 }
